eae818eff3937bdab904c1c801dbe355431d19d9
Dustin Turska <86588710+dturska12@users.noreply.github.com>
2022-06-03 00:07:02 -0500

    Update contract with mint to function

--- FlinchMob.sol
+++ FlinchMob.sol
@@ -1,34 +1,31 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: BSD-3-Clause
 pragma solidity >=0.7.0 <0.9.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import './Blimpie/Delegated.sol';
+import './Blimpie/ERC721EnumerableB.sol';
+import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract FlinchMobFreeMint is ERC721Enumerable, Ownable {
+contract FlinchMob is Delegated, ERC721Enumerable {
   using Strings for uint256;
 
   string public baseURI;
   string public baseExtension = ".json";
-  string public notRevealedUri;
   uint256 public cost = 0 ether;
-  uint256 public maxSupply = 5000;
-  uint256 public maxMintAmount = 10;
-  uint256 public nftPerAddressLimit = 100;
+  uint256 public maxSupply = 5555;
+  uint256 public maxMintAmount = 3;
+  uint256 public nftPerAddressLimit = 3;
   bool public paused = false;
   bool public revealed = false;
-  bool public onlyWhitelisted = false;
-  address[] public whitelistedAddresses;
   mapping(address => uint256) public addressMintedBalance;
 
 
   constructor(
     string memory _name,
     string memory _symbol,
-    string memory _initBaseURI,
-    string memory _initNotRevealedUri
+    string memory _initBaseURI
   ) ERC721(_name, _symbol) {
     setBaseURI(_initBaseURI);
-    setNotRevealedURI(_initNotRevealedUri);
   }
 
   // internal
@@ -43,29 +40,11 @@ contract FlinchMobFreeMint is ERC721Enumerable, Ownable {
     require(_mintAmount > 0, "need to mint at least 1 NFT");
     require(_mintAmount <= maxMintAmount, "max mint amount per session exceeded");
     require(supply + _mintAmount <= maxSupply, "max NFT limit exceeded");
-
-    if (msg.sender != owner()) {
-        if(onlyWhitelisted == true) {
-            require(isWhitelisted(msg.sender), "user is not whitelisted");
-            uint256 ownerMintedCount = addressMintedBalance[msg.sender];
-            require(ownerMintedCount + _mintAmount <= nftPerAddressLimit, "max NFT per address exceeded");
-        }
-        require(msg.value >= cost * _mintAmount, "insufficient funds");
-    }
     
     for (uint256 i = 1; i <= _mintAmount; i++) {
         addressMintedBalance[msg.sender]++;
       _safeMint(msg.sender, supply + i);
     }
-  }
-  
-  function isWhitelisted(address _user) public view returns (bool) {
-    for (uint i = 0; i < whitelistedAddresses.length; i++) {
-      if (whitelistedAddresses[i] == _user) {
-          return true;
-      }
-    }
-    return false;
   }
 
   function walletOfOwner(address _owner)
@@ -92,10 +71,6 @@ contract FlinchMobFreeMint is ERC721Enumerable, Ownable {
       _exists(tokenId),
       "ERC721Metadata: URI query for nonexistent token"
     );
-    
-    if(revealed == false) {
-        return notRevealedUri;
-    }
 
     string memory currentBaseURI = _baseURI();
     return bytes(currentBaseURI).length > 0
@@ -104,10 +79,6 @@ contract FlinchMobFreeMint is ERC721Enumerable, Ownable {
   }
 
   //only owner
-  function reveal() public onlyOwner {
-      revealed = true;
-  }
-  
   function setNftPerAddressLimit(uint256 _limit) public onlyOwner {
     nftPerAddressLimit = _limit;
   }
@@ -120,6 +91,12 @@ contract FlinchMobFreeMint is ERC721Enumerable, Ownable {
     maxMintAmount = _newmaxMintAmount;
   }
 
+  function mintTo(uint[] calldata quantity, address[] calldata recipient) external payable onlyDelegates{
+    require(quantity.length == recipient.length, "Must provide equal quantities and recipients" );
+    
+  }  
+  
+
   function setBaseURI(string memory _newBaseURI) public onlyOwner {
     baseURI = _newBaseURI;
   }
@@ -127,22 +104,9 @@ contract FlinchMobFreeMint is ERC721Enumerable, Ownable {
   function setBaseExtension(string memory _newBaseExtension) public onlyOwner {
     baseExtension = _newBaseExtension;
   }
-  
-  function setNotRevealedURI(string memory _notRevealedURI) public onlyOwner {
-    notRevealedUri = _notRevealedURI;
-  }
 
   function pause(bool _state) public onlyOwner {
     paused = _state;
-  }
-  
-  function setOnlyWhitelisted(bool _state) public onlyOwner {
-    onlyWhitelisted = _state;
-  }
-  
-  function whitelistUsers(address[] calldata _users) public onlyOwner {
-    delete whitelistedAddresses;
-    whitelistedAddresses = _users;
   }
  
   function withdraw() public payable onlyOwner {
